4a2f18743fabef5be9f9cdac33024b0b5604db05
Johann Behr <johannbehr@exng.de>
2018-06-04 20:22:23 +0200

    Created basic appdir script

--- tests/appimage.js
+++ tests/appimage.js
@@ -1,8 +1,32 @@
+// IMPORTS
+const helper = require('../lib/helper')
 const pak = require('../src/pak.js')
+const path = require('path')
+const shell = require('shelljs')
 
-// TODO: Create test with boilerplate
+var bTemp = path.join(path.sep, 'tmp', helper.rand() + '-boilerplate')
+bTemp = helper.mkdir(bTemp)
+
+var appimageTemp = path.join(path.sep, 'tmp', helper.rand() + '-appimage')
+appimageTemp = helper.mkdir(appimageTemp)
+
 module.exports = [
   () => {
-    pak.appimage()
+    // Clear temp folder to be sure
+    // shell.exec(`rm -rf ${temp}/*`)
+
+    // Clone boilerplate
+    shell.exec([
+      `cd ${bTemp}`,
+      `git clone https://github.com/ExNG/quasar-boilerplate.git . --depth 1`
+    ].join(' && '))
+
+    pak.appimage({
+      package: require(`${bTemp}/package.json`),
+      dist: `${bTemp}/dist/electron-mat/`,
+      dest: appimageTemp
+    })
+
+    console.log('appimageTemp', appimageTemp)
   }
 ]
